00fbd4b218cd087ac388832a02df64654cbbf245
Survee86 <survee86@gmail.com>
2025-05-06 22:58:44 +0300

    d

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,10 +19,12 @@ app.use (cors   ({
                 })
         )
 
-app.options('*', cors());        
+       
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
 
 // Подключение роутов
 app.use('/api/auth',    authRoutes);
